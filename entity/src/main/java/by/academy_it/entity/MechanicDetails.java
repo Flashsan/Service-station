55e27b88af10f8dee0354a7e7df306effa3c4f99
Alexander Grigorovich <algrigr@gmail.com>
2022-06-14 17:25:21 +0300

    чтото из рабочего

---
+++ entity/src/main/java/by/academy_it/entity/MechanicDetails.java
@@ -0,0 +1,52 @@
+package by.academy_it.entity;
+
+import lombok.*;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Parameter;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "mechanic_details")
+public class MechanicDetails implements Serializable {
+
+    @Id
+    @GenericGenerator(
+            name = "one-to-one(mechanic-mechanic_details",
+            strategy = "foreign",
+            parameters = @Parameter(name = "property", value = "mechanic")
+    )
+    @GeneratedValue(generator = "one-to-one(mechanic-mechanic_details")
+    @Column(name = "mechanic_id")
+    private Integer mechanicId;
+
+    @Column(name = "mechanic_experience")
+    private String mechanicExperience;
+
+    @Column(name = "mechanic_salary")
+    private String mechanicSalary;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @PrimaryKeyJoinColumn
+    private Mechanic mechanic;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        MechanicDetails that = (MechanicDetails) o;
+        return Objects.equals(mechanicId, that.mechanicId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(mechanicId, mechanicExperience, mechanicSalary);
+    }
+}
